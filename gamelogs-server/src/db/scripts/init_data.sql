17b699455e8e20c0f6190292484143bf614d0a75
Timothy Sung <tsung8@gatech.edu>
2024-11-18 20:20:32 -0500

    modify sample review desc

--- gamelogs-server/src/db/scripts/init_data.sql
+++ gamelogs-server/src/db/scripts/init_data.sql
@@ -7,7 +7,7 @@ INSERT INTO gl_user (user_id, user_name, user_biography, user_pfp_uri, num_revie
 INSERT INTO review (review_id, user_id, game_id, rating, review_description, review_date) VALUES
     (1, 9922, 201506, 4, 'good adaptation of the original web series. great liminal spaces vibe', TIMESTAMP '2024-11-12 19:15:00'),
     (2, 1, 201506, 1, 'i hate horror games, this sucks', TIMESTAMP '2024-11-13 19:15:00'),
-    (3, 9922, 114465, 5, 'fruit ninja in VR, how technology progresses...', TIMESTAMP '2024-11-14 19:15:00'),
+    (3, 9922, 114465, 5, 'minigolf in VR, how technology progresses...', TIMESTAMP '2024-11-14 19:15:00'),
     (4, 9922, 312455, 3, 'incredible game for going through a drive thru if a drive thru in real life is not possible to drive thru', TIMESTAMP '2024-11-15 19:15:00'),
     (5, 1328, 2684, 1, 'sample review', TIMESTAMP '2024-11-16 17:00:00'),
     (6, 1328, 275018, 1, 'sample review', TIMESTAMP '2024-11-16 18:00:00'),
